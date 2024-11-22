75268d0e196c8f1335cd9705d872c2c8e9e7f35e
narman85 <babayev1994@gmail.com>
2024-11-22 14:17:45 +0400

    Added environment variables setup

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,14 @@
+import sys
+import os
+
+# Import and set environment variables
+import env_vars
+
+# Add application directory to path
+path = os.path.dirname(os.path.abspath(__file__))
+if path not in sys.path:
+    sys.path.append(path)
+
 from app import app as application
 
 if __name__ == '__main__':
